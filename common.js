404a83539df9a8b56b0971cf3b1b8e061c887dff
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 12:20:18 -0800

    Refactor to MongoDB/mongoose

--- common.js
+++ common.js
@@ -1,6 +1,18 @@
-var env = require('./env.json');
+var configuration =
+{ development:
+  {
+    port: 3000,
+    mongoCredentials: 'mongodb://localhost/test'
+  },
+  production:
+  {
+    port: 80,
+    mongoCredentials: 'mongodb://deploy-shortlyDB:AfKiZYWmF7q.r.Qj2YUl8OFgCcgaI0wCCwV2VggQ6e0-@ds050077.mongolab.com:50077/deploy-shortlyDB' //<-- Insert MS mongo server URL/pass
+  }
+};
 
 exports.config = function() {
   var node_env = process.env.NODE_ENV || 'development';
-  return env[node_env];
+  return configuration[node_env];
 };
+
